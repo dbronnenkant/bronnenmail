1751131bf4ee438e0f8341bf16e1d6172467eb4a
dbronnenkant <drew@bronnenmail.com>
2018-03-11 22:29:25 -0700

    initial

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function check(input) {
+    if (input.value != document.getElementById('password').value) {
+        input.setCustomValidity('Password Must be Matching.');
+    } else {
+        // input is valid -- reset the error message
+        input.setCustomValidity('');
+    }
+}
